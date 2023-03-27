7783c291f1910aee107b3cd80a3d7455aa670695
Lê Hoàng Long <hoanglonglea1mtl@gmail.com>
2023-03-27 13:59:05 +0700

    update

--- index.js
+++ index.js
@@ -6,32 +6,37 @@ const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
 app.get('/appStoreConnectToken', async (req, res) => {
-   // const privateKey = fs.readFileSync('AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
-    const apiKeyId = 'WTZPJL3789';
-    const issuerId = '7f5deff3-b390-4625-8375-207103b7d8e4';
-    let now = Math.round((new Date()).getTime() / 1000); // Notice the /1000 
-    let nowPlus20 = now + 1199; // 1200 === 20 minutes;
-
-    const payload = {
-        'iss': issuerId,
-        'iat': now,
-        'exp': nowPlus20,
-        'aud': 'appstoreconnect-v1',
-    };
-
-
-    const signOptions = {
-        'algorithm': 'ES256',
-        header: {
-            'alg': 'ES256',
-            'kid': apiKeyId,
-            'typ': 'JWT'
-        }
-    };
-    return res.json({ token: payload, signOptions:signOptions});
-
-    // let token = jwt.sign(payload, privateKey, signOptions);
-    // return res.json({ token: token });
+    try{
+        const privateKey = fs.readFileSync('AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
+        const apiKeyId = 'WTZPJL3789';
+        const issuerId = '7f5deff3-b390-4625-8375-207103b7d8e4';
+        let now = Math.round((new Date()).getTime() / 1000); // Notice the /1000 
+        let nowPlus20 = now + 1199; // 1200 === 20 minutes;
+    
+        const payload = {
+            'iss': issuerId,
+            'iat': now,
+            'exp': nowPlus20,
+            'aud': 'appstoreconnect-v1',
+        };
+    
+    
+        const signOptions = {
+            'algorithm': 'ES256',
+            header: {
+                'alg': 'ES256',
+                'kid': apiKeyId,
+                'typ': 'JWT'
+            }
+        };
+    
+    
+        let token = jwt.sign(payload, privateKey, signOptions);
+        return res.json({ token: token });
+    }catch(e){
+        return res.json({ token: e});
+    }
+ 
 });
 
 app.get("/abc", async (req, res,) => {
