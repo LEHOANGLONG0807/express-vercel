216bc0a62680f67686d3e8bdcb3a4228a4a985d4
Lê Hoàng Long <hoanglonglea1mtl@gmail.com>
2023-03-27 13:37:02 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,36 @@ const app = express();
 const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
-app.use("/api/product", product);
+app.get('/api/appStoreConnectToken', (req, res) => {
+	const privateKey = fs.readFileSync('AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
+	const apiKeyId = 'WTZPJL3789';
+	const issuerId = '7f5deff3-b390-4625-8375-207103b7d8e4';
+	let now = Math.round((new Date()).getTime() / 1000); // Notice the /1000 
+	let nowPlus20 = now + 1199; // 1200 === 20 minutes;
 
-const PORT = process.env.PORT || 8080;
+	const payload = {
+		'iss': issuerId,
+		'iat': now,
+		'exp': nowPlus20,
+		'aud': 'appstoreconnect-v1',
+	};
+
+
+	const signOptions = {
+		'algorithm': 'ES256',
+		header: {
+			'alg': 'ES256',
+			'kid': apiKeyId,
+			'typ': 'JWT'
+		}
+	};
+
+	let token = jwt.sign(payload, privateKey, signOptions);
+	return res.json({token: token});
+});
+
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
