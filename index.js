abf9ce556c310b1233c8dafd5c0531c533be749a
Lê Hoàng Long <hoanglonglea1mtl@gmail.com>
2023-03-27 13:51:45 +0700

    add /abc

--- index.js
+++ index.js
@@ -6,32 +6,39 @@ app.use(express.json({ extended: false }));
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
-app.get('/appStoreConnectToken', (req, res) => {
-	const privateKey = fs.readFileSync('AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
-	const apiKeyId = 'WTZPJL3789';
-	const issuerId = '7f5deff3-b390-4625-8375-207103b7d8e4';
-	let now = Math.round((new Date()).getTime() / 1000); // Notice the /1000 
-	let nowPlus20 = now + 1199; // 1200 === 20 minutes;
+app.get('/appStoreConnectToken', async (req, res) => {
+    const privateKey = fs.readFileSync('AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
+    const apiKeyId = 'WTZPJL3789';
+    const issuerId = '7f5deff3-b390-4625-8375-207103b7d8e4';
+    let now = Math.round((new Date()).getTime() / 1000); // Notice the /1000 
+    let nowPlus20 = now + 1199; // 1200 === 20 minutes;
 
-	const payload = {
-		'iss': issuerId,
-		'iat': now,
-		'exp': nowPlus20,
-		'aud': 'appstoreconnect-v1',
-	};
+    const payload = {
+        'iss': issuerId,
+        'iat': now,
+        'exp': nowPlus20,
+        'aud': 'appstoreconnect-v1',
+    };
 
 
-	const signOptions = {
-		'algorithm': 'ES256',
-		header: {
-			'alg': 'ES256',
-			'kid': apiKeyId,
-			'typ': 'JWT'
-		}
-	};
+    const signOptions = {
+        'algorithm': 'ES256',
+        header: {
+            'alg': 'ES256',
+            'kid': apiKeyId,
+            'typ': 'JWT'
+        }
+    };
 
-	let token = jwt.sign(payload, privateKey, signOptions);
-	return res.json({token: token});
+    let token = jwt.sign(payload, privateKey, signOptions);
+    return res.json({ token: token });
+});
+
+app.get("/abc", async (req, res,) => {
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+    });
 });
 
 const PORT = 3000;
