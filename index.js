8020bc92345c4e1a805ce1321572359853ad3d38
Lê Hoàng Long <hoanglonglea1mtl@gmail.com>
2023-03-27 14:24:52 +0700

    update fs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 
 app.get('/appStoreConnectToken', async (req, res) => {
     try{
-        const privateKey = fs.readFileSync('./AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
+        const privateKey = fs.readFileSync('AuthKey_WTZPJL3789.p8'); // this is the file you can only download once and should treat like a real, very precious key.
         const apiKeyId = 'WTZPJL3789';
         const issuerId = '7f5deff3-b390-4625-8375-207103b7d8e4';
         let now = Math.round((new Date()).getTime() / 1000); // Notice the /1000 
@@ -37,13 +37,6 @@ app.get('/appStoreConnectToken', async (req, res) => {
         return res.json({ token: e});
     }
  
-});
-
-app.get("/abc", async (req, res,) => {
-    return res.status(200).json({
-        title: "Express Testing",
-        message: "The app is working properly!",
-    });
 });
 
 const PORT = 3000;
